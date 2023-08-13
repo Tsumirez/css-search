f7b87bd95e15742c3bf7261a53ee0c1430231f35
zefirez <tsumirez@zoho.com>
2023-08-13 21:20:02 +0200

    Now only one card can be picked at a time

--- animation.js
+++ animation.js
@@ -1,23 +1,42 @@
 let card_inputs = document.querySelectorAll("#search-results > label > input");
 
-function cardClickHandler(card) {
-    let animationSettings = { duration: 250, iterations: 1, fill: "forwards", direction: "reverse" };
+let forwardCard = null;
+let returnCard = null;
 
-    return function (e) {
-        let cardFrameCoordinates = e.target.parentElement.parentElement.getBoundingClientRect();
 
-        let pickCardAnimation = [
-            { position:"static" },
-            { left: cardFrameCoordinates.left + "px", top: cardFrameCoordinates.top + "px", position: "fixed", zIndex: 1, offset:0.001 },
-            { left: "calc(50% - 125px)", top: "calc(50% - 141px)", position: "fixed", zIndex: 1 }
-        ];
+function cardClicked(e) {
+    let card = e.target.parentElement;
 
-        animationSettings.direction = animationSettings.direction=="reverse"?"normal":"reverse";
-        card.animate(pickCardAnimation, animationSettings);
+    if(forwardCard === card) {
+        returnCard = forwardCard;
+        forwardCard = null;
+    } else if (returnCard === card) {
+        forwardCard = card;
+        returnCard = null;
+    } else {
+        returnCard = forwardCard;
+        forwardCard = card;
     }
+
+    forwardCard && animateCard(forwardCard, 'normal');
+    returnCard && animateCard(returnCard, 'reverse');
+}
+
+
+function animateCard(card, direction) {
+    let animationSettings = { duration: 250, iterations: 1, fill: "forwards" };
+    let cardFrameCoordinates = card.parentElement.getBoundingClientRect();
+
+    let pickCardAnimation = [
+        { position: "static" },
+        { left: cardFrameCoordinates.left + "px", top: cardFrameCoordinates.top + "px", position: "fixed", zIndex: 1, offset: 0.001 },
+        { left: "calc(50% - 125px)", top: "calc(50% - 141px)", position: "fixed", zIndex: 1 }
+    ];
+
+    card.animate(pickCardAnimation, { ...animationSettings, direction: direction })
 }
 
 card_inputs.forEach(input => {
     let card = input.nextElementSibling;
-    card.addEventListener("click", cardClickHandler(card))
+    card.addEventListener("click", cardClicked)
 });
